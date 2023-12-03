a6176cb8f8d6379b7511955a45cdb82165ae8390
Madan <madangowda3242@gmail.com>
2023-12-03 16:31:45 +0530

    db updated

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,14 @@
 import app from "./src/app";
 import dotenv from "dotenv";
+import { pool } from "./src/config/database";
 dotenv.config();
 
 app.listen(process.env.PORT, () => {
+    pool.connect((err) => {        
+        if (err) {
+            throw err;
+        }
+        console.log(`DB CONNECTED`);
+    })
     console.log(`Server running on ${process.env.PORT}`);
 })
--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,13 +1,39 @@
 import { Pool } from "pg";
+
 import dotenv from "dotenv";
 dotenv.config();
 
-const DATABASE_URL = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+const DATABASE_URL = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_NAME}`;
 
 export const pool = new Pool({
     connectionString: DATABASE_URL
 })
 
+const create_students = `
+    CREATE TABLE IF NOT EXISTS "students" (
+	    "stud_id" SERIAL PRIMARY KEY,
+	    "stud_name" VARCHAR(100) NOT NULL,
+	    "stud_rollno" BIGINT NOT NULL,
+        "stud_phoneno" BIGINT NOT NULL,
+        "stud_address_city" VARCHAR(100) NOT NULL,
+        "stud_address_pincode" VARCHAR(100) NOT NULL,
+        "stud_gender" VARCHAR(100) NOT NULL,
+        "year_of_enroll" VARCHAR(100) NOT NULL
+    );
+`;
+
+const create_teachers = `
+    CREATE TABLE IF NOT EXISTS "students" (
+	    "teach_id" SERIAL PRIMARY KEY,
+	    "teach_name" VARCHAR(100) NOT NULL,
+	    "teach_phoneno" BIGINT NOT NULL,
+        "teach_address_city" VARCHAR(100) NOT NULL,
+        "teach_address_pincode" VARCHAR(100) NOT NULL,
+        "teach_gender" VARCHAR(100) NOT NULL,
+        "qualification" VARCHAR(100) NOT NULL,
+        "teach_regno" VARCHAR(100) NOT NULL
+    );
+`
 export function query(text: string) {
     return new Promise((resolve, reject) => {
         pool
@@ -20,3 +46,6 @@ export function query(text: string) {
             })
     })
 }
+
+// query(create_students).then(() => console.log('Students table created')).catch((err) => console.log(err))
+// query(create_teachers).then(() => console.log('Teachers table created')).catch((err) => console.log(err))
