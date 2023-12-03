51ba38285ac295f5c13249d38b3d646b68f0d065
Madan <madangowda3242@gmail.com>
2023-12-03 10:04:07 +0530

    docker

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,6 @@ import app from "./src/app";
 import dotenv from "dotenv";
 dotenv.config();
 
-app.listen(3000, () => {
-    console.log(`Server running on 3000`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on ${process.env.PORT}`);
 })
