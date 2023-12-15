a47ea6eb6407e8a2b47caf7946ba9783c1d78fd0
Madan <madangowda3242@gmail.com>
2023-12-15 14:10:17 +0530

    color changed

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
-    <div className="h-16 p-4 bg-neutral-400 flex items-center justify-between fixed top-0 w-screen">
-      <h2 className="text-2xl font-bold">
+    <div className="h-16 p-4 bg-blue-400 flex items-center justify-between fixed top-0 w-screen">
+      <h2 className="text-2xl font-bold text-white">
         <Link to={"/"}>School Management</Link>
       </h2>
       <ul className="flex justify-center">
-        <li className="mr-4 text-lg font-medium">
+        <li className="mr-4 text-lg font-medium text-white">
           <Link to={"/students"}> Students</Link>
         </li>
-        <li className="mr-4 text-lg font-medium">
+        <li className="mr-4 text-lg font-medium text-white">
           <Link to={"/teachers"}>Teachers</Link>
         </li>
       </ul>
